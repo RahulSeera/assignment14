48329d4579a1699698ede996691308e05d3d5805
shanmukhsiva369 <dongas@iitbhilai.ac.in@gmail.com>
2024-11-16 00:47:41 +0530

    code for footer added

---
+++ footer.php
@@ -0,0 +1,11 @@
+</main>
+    <footer>
+        <div class="container">
+            <p>&copy; 2024 Work Out & Exercise. All rights reserved.</p>
+            <p>Email: info@workoutandexercise.com</p>
+            <p>Phone: +123 456 7890</p>
+        </div>
+    </footer>
+    <script src="js/script.js"></script>
+</body>
+</html>
