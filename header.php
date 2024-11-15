8d4b128512fc4ebe90a0c6da1709f2754552e3f3
Venu1524 <katariv@iitbhilai.ac.in>
2024-11-16 00:44:09 +0530

    code for header added

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Work Out & Exercise</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo">Work Out & Exercise</div>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="calculator.php">Calculator</a></li>
+                <li><a href="exercises.php">Exercises</a></li>
+                <li><a href="diet.php">Diet</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
